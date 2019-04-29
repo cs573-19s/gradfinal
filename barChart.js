ebb1aa36dbd32a98e564eeeb8e551b99827df4d5
Jason Abel <jabel@wpi.edu>
2019-04-29 00:13:00 -0400

    Finished vis and most of the final paper

--- barChart.js
+++ barChart.js
@@ -99,6 +99,61 @@ function createDamagesBarChart(svg, data){
 		.attr("height", y2.bandwidth())
 		.attr("y", y2("Damages"))
 		.attr("width", x2(0))
+		.attr("fill", "orange")
+
+	return bar;
+}
+
+
+function createHouseBarChart(svg, data){
+	var margin = {top: 20, right: 50, bottom: 30, left: 60}
+	var width = +svg.attr("width") - margin.left - margin.right
+	var height = +svg.attr("height") - margin.top - margin.bottom;
+	  
+	var sum = 0;
+	var cHurricane = ""
+
+	data.forEach(function(d){
+		if(cHurricane != d.SerialNum){
+			cHurricane = d.SerialNum
+			sum += parseInt(d.Damages) * 1000000
+		}
+	});
+
+	sum = Math.ceil (sum / 50000000000) * 50000000000
+
+	sum = Math.ceil((sum / 3100) / 20000000) * 20000000
+	  
+	x3.range([0, width])
+		.domain([0, sum]);
+
+	y3.range([height, 0])
+		.domain(["Rooms"])
+		.padding(0.1);
+	
+
+	var g = svg.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	  
+
+
+	g.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.axisBottom(x3).ticks(8).tickSizeInner([-height]));
+
+	g.append("g")
+		.attr("class", "y axis")
+		.call(d3.axisLeft(y3));
+
+	var bar = g.selectAll(".bar")
+		.data(data)
+		.enter().append("rect")
+		.attr("class", "bar")
+		.attr("x", 0)
+		.attr("height", y3.bandwidth())
+		.attr("y", y3("Rooms"))
+		.attr("width", x3(0))
 		.attr("fill", "green")
 
 	return bar;
